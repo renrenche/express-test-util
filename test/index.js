2289cbdb0514c77bb75aa3995175ef6a44081562
wangshijun2010 <wangshijun2010@gmail.com>
2016-11-03 13:24:34 +0800

    add test case

--- test/index.js
+++ test/index.js
@@ -1,10 +1,83 @@
-'use strict';
+const expect = require('chai').expect;
+const util = require('../lib');
 
-var assert = require('assert');
-var expressTestUtil = require('../lib');
+describe('#mockRequest', function () {
+    it('should have mockRequest method', function () {
+        expect(util.mockRequest).to.be.a('function');
+    });
 
-describe('express-test-util', function () {
-  it('should have unit test!', function () {
-    assert(false, 'we expected this package author to add actual unit tests.');
-  });
+    it('should `req` have standard properties', function () {
+        const req = util.mockRequest();
+
+        expect(req).to.be.a('object');
+        expect(req.query).to.be.a('object');
+        expect(req.body).to.be.a('object');
+        expect(req.session).to.be.a('object');
+        expect(req.cookies).to.be.a('object');
+        expect(req.headers).to.be.a('object');
+        expect(req.params).to.be.a('object');
+        expect(req.param).to.be.a('function');
+        expect(req.get).to.be.a('function');
+    });
+
+    it('should `req` have custome properties', function () {
+        const req = util.mockRequest({
+            props: { xxx: 'xxx' },
+        });
+
+        expect(req).to.be.a('object');
+        expect(req.query).to.be.a('object');
+        expect(req.body).to.be.a('object');
+        expect(req.session).to.be.a('object');
+        expect(req.cookies).to.be.a('object');
+        expect(req.headers).to.be.a('object');
+        expect(req.params).to.be.a('object');
+        expect(req.param).to.be.a('function');
+        expect(req.xxx).to.be.equal('xxx');
+    });
+
+    it('should `req.get` behave as expected', function () {
+        const req = util.mockRequest({
+            headers: { 'Content-Type': 'application/json' },
+            params: { key: 'value' },
+        });
+
+        expect(req.get('Content-Type')).to.equal('application/json');
+        expect(req.get('Content-Type2')).to.be.undefined;
+    });
+
+    it('should `req.param` behave as expected', function () {
+        const req = util.mockRequest({
+            params: { key: 'value' },
+        });
+
+        expect(req.param('key')).to.equal('value');
+        expect(req.param('key2')).to.be.undefined;
+    });
+});
+
+describe('#mockRequest', function () {
+    it('should have mockRequest method', function () {
+        expect(util.mockRequest).to.be.a('function');
+    });
+
+    it('should `res` have standard properties', function () {
+        const res = util.mockResponse();
+
+        expect(res).to.be.a('object');
+        expect(res.cookies).to.be.a('object');
+        expect(res.status).to.be.a('function');
+        expect(res.send).to.be.a('function');
+        expect(res.cookie).to.be.a('function');
+    });
+
+    it('should `res.cookie` behave as expected', function () {
+        const res = util.mockResponse();
+
+        expect(res).to.be.a('object');
+        expect(res.cookies).to.be.a('object');
+
+        res.cookie('key', 'value');
+        expect(res.cookies.key.value).to.equal('value');
+    });
 });
